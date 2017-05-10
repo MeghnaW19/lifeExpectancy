eddc199433afc0a6daa208c9712f4ccb6fa5356f
Meghna <meghnanvt70@gmail.com>
2017-05-10 10:20:52 +0530

    added comments and intended

--- js/lifeExpectancyMeghna.js
+++ js/lifeExpectancyMeghna.js
@@ -1,39 +1,62 @@
-let convert = function convert(startYear) {
-	if (typeof startYear === 'string') {
+/* Importing Modules*/
+let log4js = require('log4js');
+const readline = require('readline');
+const fs = require('fs');
+let logger = log4js.getLogger();
+
+/*  functions to validate start year*/
+let convert = function convert(startYear)
+{
+	if (typeof startYear === 'string')
+	{
 		return '';
 	}
-	if (typeof startYear !== 'number' || isNaN(startYear)) {
+
+	if (typeof startYear !== 'number' || isNaN(startYear))
+	{
 		throw new Error('Not a number');
 	}
-	const readline = require('readline');
-	const fs = require('fs');
+
+	/* Creating readstream */
 	const rl = readline.createInterface({
-		input: fs.createReadStream(
-			'/home/user/Desktop/expectancy/js_dev_boilerplate/inputdata/Indicators(1).csv')
-	});
+				input: fs.createReadStream('../inputdata/final.csv')
+				});
+
+	/* Variable decleration */
 	let arr = [];
 	let obj = {};
-	rl.on('line', (line) => {
+	/*  functions to read input data line by line */
+	rl.on('line', (line) =>
+	{
+		/*	To clean junk data*/
 		let cleanedLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-		//  console.log(cleanedLine);
+		// logger.debug(cleanedLine);
 		if (cleanedLine[1] === 'KHM' || cleanedLine[1] === 'AGO' ||
-			cleanedLine[1] === 'DEU' || cleanedLine[1] === 'MYS' || cleanedLine[1] === 'BTN') {
-			if (cleanedLine[3] === 'SP.DYN.LE00.FE.IN') {
+			cleanedLine[1] === 'DEU' || cleanedLine[1] === 'MYS' || cleanedLine[1] === 'BTN')
+		{
+			if (cleanedLine[3] === 'SP.DYN.LE00.FE.IN')
+			{
 				obj['CountryName'] = cleanedLine[0];
 				obj['Female'] = cleanedLine[5];
 				obj['Year'] = cleanedLine[4];
 			}
-			if (cleanedLine[3] === 'SP.DYN.LE00.MA.IN') {
+			if (cleanedLine[3] === 'SP.DYN.LE00.MA.IN')
+			{
 				obj['Male'] = cleanedLine[5];
 				arr.push(obj);
 			}
 		}
 	});
-	rl.on('close', () => {
-		//	console.log(JSON.stringify(arr, null, 4));
-		fs.writeFile('../outputdata/output.json', JSON.stringify(arr, null, 4));
-		});
+
+	rl.on('close', () =>
+	{
+	/* Creating the JSON* */
+	logger.debug(JSON.stringify(arr, null, 4));
+	fs.writeFile('../outputdata/outputlifeExpectancyMeghna.json', JSON.stringify(arr, null, 4));
+	});
+
 	return 'JSON written successfully';
 };
+
 //	convert(2011);
 module.exports = convert;
