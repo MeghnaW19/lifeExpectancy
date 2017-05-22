6cd4bc8deeac689319cee5b12316c2b093edbe78
Meghna <meghnanvt70@gmail.com>
2017-05-22 12:39:00 +0530

    Modified

--- js/lifeExpectancyMeghna.js
+++ js/lifeExpectancyMeghna.js
@@ -21,9 +21,9 @@ let convert = function convert(startYear)
 	const rl = readline.createInterface({
 				input: fs.createReadStream('../inputdata/final.csv')
 				});
-
+	const countryCodes = ['KHM', 'BTN', 'MYS', 'DEU', 'AGO'];
+	const indicators = ['SP.DYN.LE00.FE.IN', 'SP.DYN.LE00.MA.IN'];
 	/* Variable decleration */
-	let arr = [];
 	let obj = {};
 	/*  functions to read input data line by line */
 	rl.on('line', (line) =>
@@ -31,32 +31,47 @@ let convert = function convert(startYear)
 		/*	To clean junk data*/
 		let cleanedLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
 		// logger.debug(cleanedLine);
-		if (cleanedLine[1] === 'KHM' || cleanedLine[1] === 'AGO' ||
-			cleanedLine[1] === 'DEU' || cleanedLine[1] === 'MYS' || cleanedLine[1] === 'BTN')
-		{
-			if (cleanedLine[3] === 'SP.DYN.LE00.FE.IN')
-			{
-				obj['CountryName'] = cleanedLine[0];
-				obj['Female'] = cleanedLine[5];
-				obj['Year'] = cleanedLine[4];
+
+		countryCodes.forEach((countryCode)=>{
+			if(countryCode === cleanedLine[1]) {
+				if(!obj[cleanedLine[4]]) {
+					obj[cleanedLine[4]] = {
+						total: {
+							female: 0,
+							male: 0
+						}
+					};
+				}
+				if(!obj[cleanedLine[4]][cleanedLine[0]]) {
+					obj[cleanedLine[4]][cleanedLine[0]] = {};
+				}
+				indicators.forEach((indicator)=>{
+					if(indicator === cleanedLine[3]) {
+						if(indicator.split('.')[3] === 'FE') {
+							// Country specific data
+							obj[cleanedLine[4]][cleanedLine[0]]['Female'] = cleanedLine[5];
+							// adding to total
+							obj[cleanedLine[4]].total.female =
+							obj[cleanedLine[4]].total.female + parseInt(cleanedLine[5], 10);
+						} else {
+							obj[cleanedLine[4]][cleanedLine[0]]['Male'] = cleanedLine[5];
+							obj[cleanedLine[4]].total.male =
+							obj[cleanedLine[4]].total.male + parseInt(cleanedLine[5], 10);
+						}
+					}
+				});
 			}
-			if (cleanedLine[3] === 'SP.DYN.LE00.MA.IN')
-			{
-				obj['Male'] = cleanedLine[5];
-				arr.push(obj);
-			}
-		}
+		});
 	});
 
 	rl.on('close', () =>
 	{
 	/* Creating the JSON* */
-	logger.debug(JSON.stringify(arr, null, 4));
-	fs.writeFile('../outputdata/outputlifeExpectancyMeghna.json', JSON.stringify(arr, null, 4));
+	fs.writeFile('../outputdata/outputlifeExpectancyMeghna.json', JSON.stringify(obj));
 	});
 
 	return 'JSON written successfully';
 };
 
-//	convert(2011);
+		// convert(2011);
 module.exports = convert;
